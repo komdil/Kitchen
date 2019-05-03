287ed881c1ba757248b54772e796afdac4fe7f8c
Dilshod Komilov <komilovd@silkroadprofessionals.com>
2019-05-03 11:45:41 +0500

    fix assert

--- KitchenAppUnitTest/UserModelTest.cs
+++ KitchenAppUnitTest/UserModelTest.cs
@@ -1,5 +1,6 @@
 ﻿using KitchenApp.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace KitchenAppUnitTest
 {
@@ -11,11 +12,9 @@ namespace KitchenAppUnitTest
         {
             User user = new User();
             Menu menu = new Menu();
-
             user.ChooseMenu(menu);
-
             Assert.AreNotEqual(0, menu.Orders[0].Details.Count, "If some user choosed menu, than Order.OrderDetail can't be 0");
-            //Assert.IsTrue(menu.Orders[0].Details.ForEach());
+            Assert.IsTrue(menu.Orders[0].Details.Any(x => x.User == user));
         }
     }
 }
