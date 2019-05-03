befe886c70b18c0f75e531db4bb06efed1ec03d9
Behruz <Behruz@DESKTOP-0Q76MOT>
2019-05-03 13:32:29 +0500

    Create Authorization

--- KitchenApp/Controllers/HomeController.cs
+++ KitchenApp/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace KitchenApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+
     }
 }
--- KitchenApp/Startup.cs
+++ KitchenApp/Startup.cs
@@ -35,7 +35,7 @@ namespace KitchenApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=index}");
+                    template: "{controller=Home}/{action=Authorization}");
             });
         }
     }
