42b8b31d3d418f848bc74fb8097b2da1e7bc4696
karim <karim@ka.me>
2019-05-16 14:59:34 +0500

    eds

--- KitchenApp/Models/Helper.cs
+++ KitchenApp/Models/Helper.cs
@@ -10,6 +10,6 @@ namespace KitchenApp.Models
         public const string ADMIN_ROLE = "Admin";
         public const string USER_ROLE = "User";
         public const string DATABASE = "KitchenAppDb";
-        public const string CONNECTION_STRING = "Server=(localdb)\\MSSQLLocalDB;Database=KitchenAppDb;Trusted_Connection=True;";
+        public const string CONNECTION_STRING = "Server=DESKTOP-GL612FN\\SQLSERVER;Database=KitchenAppDb;Trusted_Connection=True;";
     }
 }
