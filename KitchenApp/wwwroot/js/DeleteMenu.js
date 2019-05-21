db3f572250934db763c81b95c12304728233f255
Behruz <Behruz@DESKTOP-0Q76MOT>
2019-05-21 12:48:10 +0500

    sdh

--- KitchenApp/wwwroot/js/DeleteMenu.js
+++ KitchenApp/wwwroot/js/DeleteMenu.js
@@ -1,6 +1,17 @@
-﻿function DeleteMenu(name) {
-    let answer = confirm("Do you want delete menu " + name + " ?");
-    if (answer) {
+﻿$(document).ready(function () {
+    $(".deleteMenu").on('click', function (e) {
+        e.preventDefault();
+        let answer = confirm("Do you want delete?");
+        id = $(this).data("id");
+        name = $(this).data("name");
+        alert(name);
+        if (answer) {
+           // deleteUser(id);
+        }
+    })
+
+    function deleteUser(id) {
+
         $.ajax({
             type: "Post",
             url: "/Admin/DeleteMenu",
@@ -14,4 +25,4 @@
             }
         });
     }
-}
+});
